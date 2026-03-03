a807e16feb096aa5cacf7e08afa22b20815867d4
EdoardoOldrini <Edoardo4610@gmail.com>
2026-03-03 10:57:08 +0100

    Implementata funzione somma. Manca ancora average, da testare

---
+++ simple_math.c
@@ -0,0 +1,13 @@
+/** \file simple_math.c
+	\brief a toy example of implementation of integer math. operations
+	\author Edoardo Oldrini
+*/
+
+#include "simple_math.h"
+#include <stdio.h>
+
+int sum(int a, int b){
+    return a + b;
+}
+
+
---
+++ simple_math.h
@@ -0,0 +1,16 @@
+/** \file simple_math.h
+	\brief a toy example of a header
+	\author Edoardo Oldrini
+*/
+
+
+
+#ifndef _SIMPLEMATH_
+#define _SIMPLEMATH_
+
+//somma: INPUT: 2 numeri interi; Output: 1 numero intero
+int sum(int a, int b);
+
+//media: INPUT: 2 numeri interi; Output: 1 float.
+
+#endif
